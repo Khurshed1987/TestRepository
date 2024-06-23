39f3e849924b316cff1d9375393cdebc417aa7bc
Khurshed1987 <ksavganov@gmail.com>
2024-06-23 11:03:34 -0700

    added a new line

--- src/day_01_practice/Test1.java
+++ src/day_01_practice/Test1.java
@@ -3,5 +3,6 @@ package day_01_practice;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world2");
     }
 }
