85360dd74582db33e96241973657e38c7608a1b9
Khurshed1987 <98006036+Khurshed1987@users.noreply.github.com>
2024-06-23 21:12:57 -0700

    Update Test1.java

--- src/day_01_practice/Test1.java
+++ src/day_01_practice/Test1.java
@@ -19,5 +19,11 @@ public class Test1 {
         System.out.println("Hello git and github");
 
         System.out.println("Commit from UI");
+
+        System.out.println("Hello world");
+        System.out.println("Hello world2");
+        System.out.println("Hello world3");
+        System.out.println("Hello git push");
+        //added this comment in GitHub
     }
 }
